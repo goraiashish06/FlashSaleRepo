14922d45affc2ddb7ac6b14d40c4f7c87bba2b57
ashishg <goraiashish06.gmail.com>
2019-07-08 16:35:14 +0530

    initial project version

---
+++ FlashSaleProject/src/main/java/com/sale/flashsale/FlashSaleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sale.flashsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashSaleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashSaleProjectApplication.class, args);
+	}
+
+}
